e974bd0481a0e37a16c439bb61d77291c23006b5
Maksim <maxkliment@inbox.ru>
2018-03-12 22:09:12 +0300

    Add lines for lesson 1 on Decision Trees.
    Initialise DT classifier.
    Train classifier on training data.
    Reduce number of features to 1%.
    Predict values on test data.
    Calculate accuracy.

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -24,7 +24,13 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.tree import DecisionTreeClassifier as DTC
+from sklearn.metrics import accuracy_score
 
+print len(features_train[0])
+clf = DTC(min_samples_split=40)
+clf.fit(features_train, labels_train)
+print accuracy_score(labels_test, clf.predict(features_test))
 
 #########################################################
 
