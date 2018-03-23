5c9b758db64f22d1a4f3bb153a1f7a45c1109596
maxkliment <maxkliment@inbox.ru>
2018-03-23 18:29:05 +0300

    Add lines for lesson 3 on Linear Regression.
    Read financial data.
    Extract necessary features and target.
    Initialize LinearRegression regressor.
    Fit regressor on train data.
    Calculate score on train and test data.
    Plot data and regression line.
    Fit regressor on test data.
    Calculate score on train data.
    Plot new regression line.

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -22,6 +22,7 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
+#features_list = ["bonus", "long_term_incentive"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -29,7 +30,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,7 +39,13 @@ test_color = "b"
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
+from sklearn.linear_model import LinearRegression
 
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
+print reg.coef_, reg.intercept_
+print 'Score on train data ', reg.score(feature_train, target_train)
+print 'Score on test data ', reg.score(feature_test, target_test)
 
 
 
@@ -54,7 +61,7 @@ for feature, target in zip(feature_train, target_train):
 
 ### labels for the legend
 plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
-plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
+plt.scatter(feature_train[0], target_train[0], color=train_color, label="train")
 
 
 
@@ -64,6 +71,11 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b")
+print reg.coef_, reg.intercept_
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
